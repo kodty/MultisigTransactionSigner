200abb90489b26517fe43ef8d268c7cfbbfde1dd
kailin <kailin_5@hotmail.com>
2017-08-29 20:11:47 +0100

    updated to new sdk

--- XEMSign/Tasks/RuleScanner.cs
+++ XEMSign/Tasks/RuleScanner.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CSharp2nem;
+using CSharp2nem.Connectivity;
+using CSharp2nem.Utils;
+using CSharp2nem.ResponseObjects.Transaction;
+using CSharp2nem.Model.AccountSetup;
+using CSharp2nem.RequestClients;
 
 namespace XEMSign
 {
@@ -24,7 +29,7 @@ namespace XEMSign
            if (whiteList != null)
                foreach (MyConfigInstanceElement b in whiteList.Instances)
                {           
-                   _white.Add(b.Code.GetResultsWithoutHyphen());
+                   _white.Add(StringUtils.GetResultsWithoutHyphen(b.Code));
                }
             
             var blackList = ConfigurationManager.GetSection("accountsBlackList")
@@ -34,106 +39,110 @@ namespace XEMSign
             {
                 foreach (MyConfigInstanceElement b in blackList.Instances)
                 {
-                    _black.Add(b.Code.GetResultsWithoutHyphen());
+                    _black.Add(StringUtils.GetResultsWithoutHyphen(b.Code));
                 }
             }
         }
 
-        internal  bool ScanTxAgainstRuleSet(Transactions.TransactionData t, VerifiableAccount acc)
+        internal  bool ScanTxAgainstRuleSet(Transactions.TransactionData t)
         {
-            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestNet();
+            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestnet();
             else Con.SetMainnet();
 
-            var multisigAccInfo = new AccountFactory(Con).FromPublicKey(t.transaction.otherTrans.signer);
+            var multisigAccInfo = new AccountClient(Con);
 
-            var accInfo = multisigAccInfo.GetAccountInfoAsync().Result;
+            var valid = true;
 
-            if (_black.Count > 0 && _black.Contains(t.transaction.otherTrans.recipient.GetResultsWithoutHyphen()))
-            {
-                Console.WriteLine("failed on blacklist");
-                return false;
-            }
+            multisigAccInfo.BeginGetAccountInfoFromPublicKey(ar => {
+
+
+                if (_black.Count > 0 && _black.Contains(StringUtils.GetResultsWithoutHyphen(t.transaction.otherTrans.recipient)))
+                {
+                    Console.WriteLine("failed on blacklist");
+                    valid = false;
+                }
             
-            if (_white.Count > 0 && !_white.Contains(t.transaction.otherTrans.recipient.GetResultsWithoutHyphen()))
-            {
-                Console.WriteLine("failed on white list");
-                return false;
-            }
+                if (_white.Count > 0 && !_white.Contains(StringUtils.GetResultsWithoutHyphen(t.transaction.otherTrans.recipient)))
+                {
+                        Console.WriteLine("failed on white list");
+                        valid = false;
+                }
 
             
-            if (long.Parse(ConfigurationManager.AppSettings["maxTx"]) != 0 && t.transaction.otherTrans.amount >= long.Parse(ConfigurationManager.AppSettings["maxTx"]) * 1000000)
-            {
-                Console.WriteLine("failed max transaction size");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["maxTx"]) != 0 && t.transaction.otherTrans.amount >= long.Parse(ConfigurationManager.AppSettings["maxTx"]) * 1000000)
+                {
+                    Console.WriteLine("failed max transaction size");
+                    valid = false;
+                }
 
-            if (long.Parse(ConfigurationManager.AppSettings["minTx"]) != 0 &&t.transaction.otherTrans.amount <= long.Parse(ConfigurationManager.AppSettings["minTx"]) * 1000000)
-            {
-                Console.WriteLine("failed on min transaction size");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["minTx"]) != 0 &&t.transaction.otherTrans.amount <= long.Parse(ConfigurationManager.AppSettings["minTx"]) * 1000000)
+                {
+                    Console.WriteLine("failed on min transaction size");
+                    valid = false;
+                }
 
-            if (long.Parse(ConfigurationManager.AppSettings["maxBal"]) != 0 && accInfo.Account.Balance >= long.Parse(ConfigurationManager.AppSettings["maxBal"]) * 1000000)
-            {
-                Console.WriteLine("failed on max balance");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["maxBal"]) != 0 && ar.Content.Account.Balance >= long.Parse(ConfigurationManager.AppSettings["maxBal"]) * 1000000)
+                {
+                    Console.WriteLine("failed on max balance");
+                    valid = false;
+                }
 
-            if (long.Parse(ConfigurationManager.AppSettings["minBal"]) != 0 && accInfo.Account.Balance <= long.Parse(ConfigurationManager.AppSettings["minBal"]) * 1000000)
-            {
-                Console.WriteLine("failed on min balance");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["minBal"]) != 0 && ar.Content.Account.Balance <= long.Parse(ConfigurationManager.AppSettings["minBal"]) * 1000000)
+                {
+                    Console.WriteLine("failed on min balance");
+                    valid = false;
+                }
 
-            if (ConfigurationManager.AppSettings["secretCode"] != "" && t.transaction.otherTrans.message.payload != ConfigurationManager.AppSettings["secretCode"])
-            {
-                Console.WriteLine("failed on secret code");
-                return false;
-            }
+                if (ConfigurationManager.AppSettings["secretCode"] != "" && t.transaction.otherTrans.message.payload != ConfigurationManager.AppSettings["secretCode"])
+                {
+                    Console.WriteLine("failed on secret code");
+                    valid = false;
+                }
 
-            var sum1 = TxSummaryController.GetSummaryForAccount(multisigAccInfo.Address.Encoded, 1);
+                var sum1 = TxSummaryController.GetSummaryForAccount(ar.Content.Account.Address, 1);
             
-            if (int.Parse(ConfigurationManager.AppSettings["maxDayTx"]) != 0 && sum1 != null && sum1.Count >= int.Parse(ConfigurationManager.AppSettings["maxDayTx"]))
-            {
-                Console.WriteLine("failed on daily max transactions");
-                return false;
-            }
+                if (int.Parse(ConfigurationManager.AppSettings["maxDayTx"]) != 0 && sum1 != null && sum1.Count >= int.Parse(ConfigurationManager.AppSettings["maxDayTx"]))
+                {
+                    Console.WriteLine("failed on daily max transactions");
+                    valid = false;
+                }
 
-            var sum7 = TxSummaryController.GetSummaryForAccount(multisigAccInfo.Address.Encoded, 7);
+                var sum7 = TxSummaryController.GetSummaryForAccount(ar.Content.Account.Address, 7);
 
-            if (int.Parse(ConfigurationManager.AppSettings["maxWeekTx"]) != 0 && sum7 != null && sum7.Count >= int.Parse(ConfigurationManager.AppSettings["maxWeekTx"]))
-            {
-                Console.WriteLine("failed on weekly max transactions");
-                return false;
-            }
+                if (int.Parse(ConfigurationManager.AppSettings["maxWeekTx"]) != 0 && sum7 != null && sum7.Count >= int.Parse(ConfigurationManager.AppSettings["maxWeekTx"]))
+                {
+                    Console.WriteLine("failed on weekly max transactions");
+                    valid = false;
+                }
 
-            var sum31 = TxSummaryController.GetSummaryForAccount(multisigAccInfo.Address.Encoded, 31);
+                var sum31 = TxSummaryController.GetSummaryForAccount(ar.Content.Account.Address, 31);
 
-            if (int.Parse(ConfigurationManager.AppSettings["maxMonthTx"]) != 0 && sum31 != null && sum31.Count >= int.Parse(ConfigurationManager.AppSettings["maxMonthTx"]))
-            {
-                Console.WriteLine("failed on monthly max transactions");
-                return false;
-            }
+                if (int.Parse(ConfigurationManager.AppSettings["maxMonthTx"]) != 0 && sum31 != null && sum31.Count >= int.Parse(ConfigurationManager.AppSettings["maxMonthTx"]))
+                {
+                    Console.WriteLine("failed on monthly max transactions");
+                    valid = false;
+                }
 
-            if (long.Parse(ConfigurationManager.AppSettings["maxDayAmount"]) != 0 && sum1 != null && sum1.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxDayAmount"]) * 1000000)
-            {
-                Console.WriteLine("failed on daily max transactions");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["maxDayAmount"]) != 0 && sum1 != null && sum1.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxDayAmount"]) * 1000000)
+                {
+                    Console.WriteLine("failed on daily max transactions");
+                    valid = false;
+                }
 
-            if (long.Parse(ConfigurationManager.AppSettings["maxWeekAmount"]) != 0 && sum7 != null && sum7.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxWeekAmount"]) * 1000000)
-            {
-                Console.WriteLine("failed on weekly max transactions");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["maxWeekAmount"]) != 0 && sum7 != null && sum7.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxWeekAmount"]) * 1000000)
+                {
+                    Console.WriteLine("failed on weekly max transactions");
+                    valid = false;
+                }
             
-            if (long.Parse(ConfigurationManager.AppSettings["maxMonthAmount"]) != 0 && sum31 != null && sum31.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxMonthAmount"]) * 1000000)
-            {
-                Console.WriteLine("failed on monthly max transactions");
-                return false;
-            }
+                if (long.Parse(ConfigurationManager.AppSettings["maxMonthAmount"]) != 0 && sum31 != null && sum31.Sum(tx => tx.Amount) >= long.Parse(ConfigurationManager.AppSettings["maxMonthAmount"]) * 1000000)
+                {
+                    Console.WriteLine("failed on monthly max transactions");
+                    valid = false;
+                }
+            }, t.transaction.otherTrans.signer).AsyncWaitHandle.WaitOne();
 
-            return true;
+            return valid;
         }
     }
 }
--- XEMSign/Tasks/TaskRunner.cs
+++ XEMSign/Tasks/TaskRunner.cs
@@ -6,24 +6,29 @@ using System.Threading.Tasks;
 using System.Configuration;
 using CSharp2nem;
 using Newtonsoft.Json;
-
+using CSharp2nem.CryptographicFunctions;
+using CSharp2nem.Model.AccountSetup;
+using CSharp2nem.Connectivity;
+using CSharp2nem.RequestClients;
+using CSharp2nem.ResponseObjects.Transaction;
+using CSharp2nem.Model.DataModels;
 
 namespace XEMSign
 {
     internal class KeyLastCheckedPair
     {
         internal  List<string> CheckedHash { get; set; }
-        internal VerifiableAccount Acc { get; set; }
+        internal PrivateKeyAccountClient Acc { get; set; }
     }
 
     internal class AccountScanner
     {
         private static readonly Connection Con = new Connection();
-        internal async void ScanAccounts()
+        internal void ScanAccounts()
         {
             var privateKeys = ConfigurationManager.GetSection("accountsPrivate") as MyConfigSection;
 
-            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestNet();
+            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestnet();
             else Con.SetMainnet();
 
             List<KeyLastCheckedPair> listOfCosigs = new List<KeyLastCheckedPair>();
@@ -34,7 +39,7 @@ namespace XEMSign
 
                 pair.CheckedHash = new List<string>();
 
-                pair.Acc = new AccountFactory(Con).FromPrivateKey(e.Code);
+                pair.Acc = new PrivateKeyAccountClientFactory(Con).FromPrivateKey(e.Code);
 
                 listOfCosigs.Add(pair);
             }
@@ -43,11 +48,41 @@ namespace XEMSign
             {
                 foreach (var pair in listOfCosigs)
                 {
-                    Transactions.All transactions;
-
                     try
                     {
-                        transactions = await pair.Acc.GetUnconfirmedTransactionsAsync();
+                        var client = new TransactionDataClient(Con);
+
+                        client.BeginGetUnconfirmedTransactions(ar => {
+                            if (pair.CheckedHash.Count > 0 && ar.Content.data.Count > 0)
+                            {
+                                pair.CheckedHash.RemoveAll(e => ar.Content.data.All(i => e != i.meta.data));
+                            }
+
+
+                            foreach (var t in ar.Content.data)
+                            {
+
+                                if (t.transaction.type != 4100 || t.transaction?.otherTrans?.type != 257 || pair.CheckedHash.Contains(t.meta.data)) continue;
+
+                                if (t.transaction.signer == pair.Acc.PublicKey.Raw) continue;
+
+                                Console.WriteLine();
+
+                                Console.WriteLine("checking transaction: \n" + t.meta.data);
+
+                                pair.CheckedHash.Add(t.meta.data);
+
+                                Console.WriteLine("checked");
+
+                                if (new RuleScanner().ScanTxAgainstRuleSet(t))
+                                    SignTransaction(
+                                        acc: pair.Acc,
+                                        t: t,
+                                        multisigAcc: AddressEncoding.ToEncoded(
+                                            network: 0x90,
+                                            publicKey: new PublicKey(t.transaction.otherTrans.signer)));
+                            }
+                        },pair.Acc.Address.Encoded).AsyncWaitHandle.WaitOne();          
                     }
                     catch (Exception e)
                     {
@@ -56,65 +91,43 @@ namespace XEMSign
                     }
                  
 
-                   if (pair.CheckedHash.Count > 0 && transactions.data.Count > 0)
-                   {
-                       pair.CheckedHash.RemoveAll(e => transactions.data.All(i => e != i.meta.data));
-                   }
-                    
-
-                    foreach (var t in transactions.data)
-                    {
-                        
-                        if (t.transaction.type != 4100 || t.transaction?.otherTrans?.type != 257 || pair.CheckedHash.Contains(t.meta.data)) continue;
-
-                        if (t.transaction.signer == pair.Acc.PublicKey.Raw) continue;
-
-                        Console.WriteLine();
-
-                        Console.WriteLine("checking transaction: \n" + t.meta.data);
-
-                        pair.CheckedHash.Add(t.meta.data);
-
-                        Console.WriteLine("checked");
-
-                        if (new RuleScanner().ScanTxAgainstRuleSet(t, pair.Acc))
-                            SignTransaction(
-                                acc: pair.Acc, 
-                                t: t, 
-                                multisigAcc: AddressEncoding.ToEncoded(
-                                    network: 0x90, 
-                                    publicKey: new PublicKey(t.transaction.otherTrans.signer)));
-                    }
+                   
                 }     
             }
         }
 
-        private static async void SignTransaction(VerifiableAccount acc, Transactions.TransactionData t, string multisigAcc)
+        private static void SignTransaction(PrivateKeyAccountClient acc, Transactions.TransactionData t, string multisigAcc)
         {
             Console.WriteLine("signing transaction");
             try
             {
-                var a = await acc.SignMultisigTransaction(new MultisigSignatureTransactionData
-                {
-                    Deadline = int.Parse(ConfigurationManager.AppSettings["deadline"]) == 0
-                 ? 82800 : int.Parse(ConfigurationManager.AppSettings["deadline"]),
-                    TransactionHash = t.meta.data,
-                    MultisigAddress = new Address(multisigAcc)
-                });
-                if (a.Code == 1)
-                {
-                    var sum = new TxSummary()
-                    {
-                        AccAddress = multisigAcc,
-                        DateOfTx = DateTime.Now,
-                        Amount = t.transaction.otherTrans.amount
-                    };                  
-                    TxSummaryController.AddSummaryForAccount(sum);
-                }
+                  acc.BeginSignatureTransactionAsync(ar =>{
 
-                Console.WriteLine(a.Message);
-                Console.WriteLine();
-                Console.WriteLine();
+                      if (ar.Content.Code == 1)
+                      {
+                          var sum = new TxSummary()
+                          {
+                              AccAddress = multisigAcc,
+                              DateOfTx = DateTime.Now,
+                              Amount = t.transaction.otherTrans.amount
+                          };
+
+                          TxSummaryController.AddSummaryForAccount(sum);
+                      }
+
+                      Console.WriteLine(ar.Content.Message);
+                      Console.WriteLine();
+                      Console.WriteLine();
+
+                  }, new MultisigSignatureTransactionData
+                  {
+                      Deadline = int.Parse(ConfigurationManager.AppSettings["deadline"]) == 0
+                      ? 82800 : int.Parse(ConfigurationManager.AppSettings["deadline"]),
+                      TransactionHash = t.meta.data,
+                      MultisigAddress = new Address(multisigAcc)
+
+                  }).AsyncWaitHandle.WaitOne();
+                 
             }
             catch (Exception e)
             {
