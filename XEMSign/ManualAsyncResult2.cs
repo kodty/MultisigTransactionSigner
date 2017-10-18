03c949b74a483ae171b213735842f2519b757309
DESKTOP-TBVFA58\kaili <kailin_5@hotmail.com>
2017-10-18 01:31:40 +0100

    update libraries. add support for ico module payout checks.

---
+++ XEMSign/ManualAsyncResult2.cs
@@ -0,0 +1,107 @@
+﻿/*
+ 
+ Credit to Jaguar for contributing this class
+ 
+ */
+
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Threading;
+
+namespace XEMSign
+{
+    /// <summary>
+    /// A custom implementation of IAsyncResult.
+    /// </summary>
+    /// <remarks>
+    /// Used to track and transmit data between BeginX and EndX methods as well as any asynchronous operations assigned to ManualAsyncResult internally. When the AsyncWaitHandle.WaitOne function is used, it waits for all internal asynchronous operations to complete to prevent premeture thread release.
+    /// </remarks>
+    public class ManualAsyncResult2 : IAsyncResult
+    {
+        internal ManualAsyncResult2()
+        {
+            m_waitHandle = new EventWaitHandle(false, EventResetMode.ManualReset);
+        }
+
+        internal string Path { get; set; }
+        internal string Query { get; set; }
+        internal byte[] Bytes { get; set; }
+
+        internal HttpWebRequest HttpWebRequest { get; set; }
+
+        /// <summary>
+        /// Gets a user-defined object that qualifies or contains information about an asynchronous operation.
+        /// </summary>
+        /// 
+        internal WebResponse Response { get; set; }
+        public object AsyncState { get; set; }
+        /// <summary>
+        /// Gets a <see cref="T:System.Threading.WaitHandle" /> that is used to wait for an asynchronous operation to complete.
+        /// </summary>
+        public WaitHandle AsyncWaitHandle => m_waitHandle;
+        /// <summary>
+        /// Gets a value that indicates whether the asynchronous operation completed synchronously.
+        /// </summary>
+        public bool CompletedSynchronously => false;
+        /// <summary>
+        /// Gets a value that indicates whether the asynchronous operation has completed.
+        /// </summary>
+        public bool IsCompleted => m_waitHandle.WaitOne(0);
+        /// <summary>
+        /// Gets the error.
+        /// </summary>
+        /// <value>
+        /// The error.
+        /// </value>
+        public Exception Error { get; set; }
+
+        internal AsyncCallback WrapHandler(AsyncCallback callback)
+        {
+            Interlocked.Increment(ref m_outstandingCallbacks);
+
+            return ar => {
+                try
+                {
+                    callback(ar);
+
+
+                    if (0 == Interlocked.Decrement(ref m_outstandingCallbacks))
+                    {
+                        m_waitHandle.Set();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Error = ex;
+
+                    m_waitHandle.Set();
+                }
+            };
+        }
+
+        internal long TimeOutWait()
+        {
+            var watch = new Stopwatch();
+
+            watch.Start();
+
+            AsyncWaitHandle.WaitOne(5000);
+
+            watch.Stop();
+
+            if (watch.ElapsedMilliseconds >= 5000)
+            {
+                HttpWebRequest.Abort();
+
+                throw new Exception("Timed Out");
+            }
+
+            return watch.ElapsedMilliseconds;
+        }
+
+        internal EventWaitHandle m_waitHandle;
+
+        internal int m_outstandingCallbacks;
+    }
+}
--- XEMSign/Tasks/RuleScanner.cs
+++ XEMSign/Tasks/RuleScanner.cs
@@ -10,6 +10,11 @@ using CSharp2nem.Utils;
 using CSharp2nem.ResponseObjects.Transaction;
 using CSharp2nem.Model.AccountSetup;
 using CSharp2nem.RequestClients;
+using CSharp2nem.CryptographicFunctions;
+using Newtonsoft.Json;
+using Chaos.NaCl;
+using System.Net;
+using System.IO;
 
 namespace XEMSign
 {
@@ -23,7 +28,10 @@ namespace XEMSign
 
         public RuleScanner()
         {
-           var whiteList = ConfigurationManager.GetSection("accountsWhiteList")
+            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestnet();
+            else Con.SetMainnet();
+
+            var whiteList = ConfigurationManager.GetSection("accountsWhiteList")
                as MyConfigSection;
 
            if (whiteList != null)
@@ -35,19 +43,20 @@ namespace XEMSign
             var blackList = ConfigurationManager.GetSection("accountsBlackList")
                 as MyConfigSection;
 
-            if (blackList == null) return;
+            if (blackList != null);
             {
                 foreach (MyConfigInstanceElement b in blackList.Instances)
                 {
                     _black.Add(StringUtils.GetResultsWithoutHyphen(b.Code));
                 }
             }
+
+            
         }
 
         internal  bool ScanTxAgainstRuleSet(Transactions.TransactionData t)
         {
-            if (ConfigurationManager.AppSettings["network"] == "test") Con.SetTestnet();
-            else Con.SetMainnet();
+            
 
             var multisigAccInfo = new AccountClient(Con);
 
@@ -140,9 +149,173 @@ namespace XEMSign
                     Console.WriteLine("failed on monthly max transactions");
                     valid = false;
                 }
-            }, t.transaction.otherTrans.signer).AsyncWaitHandle.WaitOne();
+                
+                if(ConfigurationManager.AppSettings["ICOAccountPubKey"] != "")
+                {
+                    valid = false;
+                
+                    var TransactionDataClient = new TransactionDataClient(Con);
+                                
+                    // get all incoming transactions to the ICO deposit account
+                    TransactionDataClient.BeginGetIncomingTransactions(ar2 => {
 
+                        // fail if deposit hash not present in payout transaction
+                        if ((t.transaction.otherTrans == null ? t.transaction.message?.payload : t.transaction.otherTrans.message?.payload) != null)
+                        {
+                            // find deposit transaction based on hash in message
+                            var tx = ar2.Content.data.Single(x => x.meta.hash.data == Encoding.UTF8.GetString(CryptoBytes.FromHexString((t.transaction.type == 257 ? t.transaction.message.payload : t.transaction.otherTrans.message.payload))));
+
+                            // initiate mosaic client
+                            var mosaicClient = new NamespaceMosaicClient(Con);
+
+                            var mosaicDivisibility = 0;
+
+                            // retrieve mosaic definition for rate calculation
+                            mosaicClient.BeginGetMosaicsByNameSpace(ar3 =>
+                            {
+                                if (ar3.Ex != null)
+                                {
+                                    Console.WriteLine(ar2.Ex);
+                                } // if no error, set mosaic divisibility
+                                else mosaicDivisibility = int.Parse(ar3.Content.Data[0].Mosaic.Properties[0].Value);
+
+                                var depositedAmount = tx.transaction.type == 257
+                                            ? tx.transaction.amount
+                                            : tx.transaction.otherTrans.amount;
+
+                                if ((tx.transaction.mosaics ?? tx.transaction.otherTrans.mosaics) != null)
+                                {
+                                    foreach (var m in tx.transaction.mosaics ?? tx.transaction.otherTrans.mosaics)
+                                    {
+                                        if (m.mosaicId.name == "xem" && m.mosaicId.namespaceId == "nem")
+                                        {
+                                            depositedAmount = m.quantity;
+                                        }
+                                    }
+                                }
+
+                                // calculate based on pre-set currency denomination
+                                if (ConfigurationManager.AppSettings["currency"] == "USD")
+                                {
+                                    // fail if no mosaics present in ico payout transaction
+                                    if (t.transaction.otherTrans.mosaics != null )
+                                    {
+                                        // check if the mosaic quantity paid out is correct for the deposit
+                                        var amountValid = RateCalculation(depositedAmount, mosaicDivisibility) == t.transaction.otherTrans.mosaics[0].quantity;
+
+                                        // check the recipient of the payout is the signer of the deposit, fail if not.
+                                        if (amountValid && t.transaction.otherTrans.recipient == AddressEncoding.ToEncoded(Con.GetNetworkVersion(), new PublicKey(tx.transaction.otherTrans?.signer == null ? tx.transaction.signer : tx.transaction.otherTrans.signer)))
+                                        {
+                                            valid = true;
+
+                                        }
+                                        else Console.WriteLine("failed on transaction verification: amount or recipient is invalid");
+                                    }
+                                    else Console.WriteLine("failed on transaction verification: invalid or missing mosaic");
+                                    
+                                }
+                                else if (ConfigurationManager.AppSettings["currency"] == "XEM")
+                                {
+                                    // set mosaic amount paid out.
+                                    var q = t.transaction.otherTrans.mosaics[0].quantity;
+
+                                    // get amount that should be paid out for the deposit
+                                    var a = long.Parse(ConfigurationManager.AppSettings["currency"]) / depositedAmount;
+
+                                    // if incorrect, fail
+                                    valid = a == q ? true : false;
+
+                                    if (!valid) Console.WriteLine("failed on transaction verification: amounts do not match");
+                                }
+
+                            }, ConfigurationManager.AppSettings["namespace"], ConfigurationManager.AppSettings["ID"]).AsyncWaitHandle.WaitOne();
+
+                        }
+                        else Console.WriteLine("failed on transaction verification: missing hash");
+
+
+                    }, AddressEncoding.ToEncoded(Con.GetNetworkVersion(), new PublicKey(ConfigurationManager.AppSettings["ICOAccountPubKey"]))).AsyncWaitHandle.WaitOne();
+                }
+            }, t.transaction.otherTrans.signer).AsyncWaitHandle.WaitOne();
+            
             return valid;
         }
+
+        private static long RateCalculation(long xem, int divisibility)
+        {
+            double rate = 0.0;
+
+            UriBuilder uri = new UriBuilder()
+            {
+                Host = "api.coinmarketcap.com",
+                Path = "/v1/ticker/nem",
+                Query = "convert=USD"
+            };
+
+            var Con2 = new Connection(uri);
+
+            var Http = (HttpWebRequest)WebRequest.Create(Con2.Uri.Uri.AbsoluteUri);
+
+            Http.Accept = "application/json";
+
+            var asyncResult = new ManualAsyncResult2();
+
+            Http.BeginGetResponse(asyncResult.WrapHandler(ar =>
+            {
+                try
+                {
+                    var response = Http.EndGetResponse(ar);
+
+                    Stream responseStream = response.GetResponseStream();
+
+                    var currencyData = JsonConvert.DeserializeObject<List<currenyData>>(new StreamReader(responseStream).ReadToEnd());
+
+                    rate = double.Parse(currencyData[0].price_usd);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }), null);
+
+            // wait for callback to complete
+            asyncResult.AsyncWaitHandle.WaitOne();
+
+            // calculate amount of xar to return
+            var amount = xem / (double.Parse(ConfigurationManager.AppSettings["cost"]) / rate);
+
+            // round up to nearest decimal based on mosaic divisibility
+            amount = (Math.Ceiling(amount / Math.Pow(10, 6 - divisibility)));
+           
+            return (long)BonusCalculation(amount);
+        }
+
+        private static double BonusCalculation(double amount)
+        {
+            return DateTime.Now < DateTime.Parse(ConfigurationManager.AppSettings["bonusDate1"]) ? amount + (amount / 100 * long.Parse(ConfigurationManager.AppSettings["bonus1"]))
+                 : DateTime.Now < DateTime.Parse(ConfigurationManager.AppSettings["bonusDate2"]) ? amount + (amount / 100 * long.Parse(ConfigurationManager.AppSettings["bonus2"]))
+                 : DateTime.Now < DateTime.Parse(ConfigurationManager.AppSettings["bonusDate3"]) ? amount + (amount / 100 * long.Parse(ConfigurationManager.AppSettings["bonus3"]))
+                 : amount;
+        }
+    }
+
+    public class currenyData
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public string symbol { get; set; }
+        public string rank { get; set; }
+        public string price_usd { get; set; }
+        public string price_btc { get; set; }
+        [JsonProperty("24h_volume_usd")]
+        public string volumeusd { get; set; }
+        public string market_cap_usd { get; set; }
+        public string available_supply { get; set; }
+        public string total_supply { get; set; }
+        public string percent_change_1h { get; set; }
+        public string percent_change_24h { get; set; }
+        public string percent_change_7d { get; set; }
+        public string last_updated { get; set; }
     }
 }
+
